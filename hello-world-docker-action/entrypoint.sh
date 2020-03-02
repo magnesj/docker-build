4754d576a1fcd80a27fc14ca3ac290de7df4bb7d
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2020-03-02 15:41:05 +0100

    git cmake

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -7,4 +7,10 @@ echo ::set-output name=time::$time
 yum install -y qt5-qtbase
 yum install -y qt5-qtbase-devel
 yum install -y qt5-qtscript-devel
+yum install -y cmake
+yum install -y git
 
+cmake --version
+git --version
+
+git checkout https://github.com/OPM/ResInsight.git
