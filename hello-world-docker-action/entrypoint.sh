e937d32d68ab2098abf2307967d4e1187d98cd0d
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2020-03-02 15:33:46 +0100

    sdf

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -4,7 +4,7 @@ echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
 
-yum install qt5-qtbase
-yum install qt5-qtbase-devel
-yum install qt5-qtscript-devel
+yum install -y qt5-qtbase
+yum install -y qt5-qtbase-devel
+yum install -y qt5-qtscript-devel
 
